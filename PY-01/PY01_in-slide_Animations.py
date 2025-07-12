022236534470b85a58809b645ab541794c13478d
R-kaloustian <102569106+Roupstrer02@users.noreply.github.com>
2025-07-12 11:46:38 -0400

    Update PY01_in-slide_Animations.py

--- PY-01/PY01_in-slide_Animations.py
+++ PY-01/PY01_in-slide_Animations.py
@@ -2,30 +2,17 @@ import pygame as pg
 
 #PY01 - Class2
 pg.init()
-screen = pg.display.set_mode((800,800))
+screen = pg.display.set_mode((800,400))
 clock = pg.time.Clock()
-myRect = pg.Rect(50,50,15,15)
-
+myRect = pg.Rect(50,100,150,200)
 black = (0,0,0)
 green = (0,255,0)
-
-font = pg.font.Font('freesansbold.ttf', 32)
 Xdirection = 1
-Ydirection = 1
-
 while True:
     myRect.x += Xdirection
-    myRect.y += Ydirection
-
-    if myRect.x <= 0 or myRect.x >= 785:
+    if myRect.x <= 0 or myRect.x >= 650:
         Xdirection = -Xdirection
-    if myRect.y <= 0 or myRect.y >= 785:
-        Ydirection = -Ydirection
-    text = font.render("myRect.x = " + str(myRect.x), True, "white")
-    text2 = font.render("myRect.y = " + str(myRect.y), True, "white")
     screen.fill(black)
     pg.draw.rect(screen, green, myRect)
-    screen.blit(text, (0,0))
-    screen.blit(text2, (0,32))
     pg.display.flip()
     clock.tick(60)
