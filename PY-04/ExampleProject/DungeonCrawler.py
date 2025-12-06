47e82a7b812f630b7734a9c6ea9910411ba2fa77
Roupstrer02 <102569106+Roupstrer02@users.noreply.github.com>
2025-12-05 20:49:32 -0500

    updated DungeonCrawler

--- PY-04/ExampleProject/DungeonCrawler.py
+++ PY-04/ExampleProject/DungeonCrawler.py
@@ -16,38 +16,97 @@ class player:
     down = pg.K_s
     left = pg.K_a
     right = pg.K_d
-    speed = 5
-
+    speed = 20
+    room_x = 0
+    room_y = 0
+    position_x = 500
+    position_y = 500
     def control(self):
         keys = pg.key.get_pressed()
 
+
+        cam_x = 0
+        cam_y = 0
         if keys[self.up]:
-            self.hitbox.y -= self.speed
+            cam_y += self.speed
         if keys[self.down]:
-            self.hitbox.y += self.speed
+            cam_y -= self.speed
         if keys[self.left]:
-            self.hitbox.x -= self.speed
+            cam_x += self.speed
         if keys[self.right]:
-            self.hitbox.x += self.speed
+            cam_x -= self.speed
+
+        self.position_x -= cam_x
+        self.position_y -= cam_y
+        self.room_x = self.position_x // 1000
+        self.room_y = self.position_y // 1000
+        return cam_x, cam_y
 
     def draw(self):
         pg.draw.rect(screen, 'lightblue', self.hitbox)
 
-class room:
-    tiles = []
+class Room:
+    def __init__(self, roomtype, rx, ry, px, py):
+        dx = 1000*rx - px + 500
+        dy = 1000*ry - py + 500
+        if roomtype == "standard":
+            self.tiles = [pg.Rect(dx + 100, dy + 100, 800, 800),
+            pg.Rect(dx + 900, dy + 450,100,100),
+            pg.Rect(dx + 450, dy + 900,100,100)]
+
+            if rx > 0:
+                self.tiles.append(pg.Rect(dx + 0, dy + 450,100,100))
+            if ry > 0:
+                self.tiles.append(pg.Rect(dx + 450, dy + 0,100,100))
+    def draw(self):
+        for tile in self.tiles:
+            pg.draw.rect(screen, "brown", tile)
+
+
+def moveCamera(d, x, y):
+    for row in d:
+        for room in row:
+            if room is not None:
+                for tile in room.tiles:
+                    tile.x += x
+                    tile.y += y
+
+def updateDungeon(d, p, x, y):
+    if y > len(d)-1:
+        #d.append([None for x in range(len(d), y-1)])
+        d.append([Room("standard", x, y, p.position_x, p.position_y)])
+    elif x > len(d[y])-1:
+        for i in range(len(d[y]), x):
+            d[y].append(None)
+        d[y].append(Room("standard", x, y, p.position_x, p.position_y))
+    elif d[y][x] is None:
+        d[y][x] = Room("standard", x, y, p.position_x, p.position_y)
+
 
 
 p = player()
+dungeon = [[Room("standard", 0, 0, p.position_x, p.position_y)]]
+enemies = []
 while True:
     pg.event.pump()
-
-    p.control()
-
+    roomBefore = (p.room_x, p.room_y)
+    cx, cy = p.control()
+    roomAfter = (p.room_x, p.room_y)
+    moveCamera(dungeon, cx, cy)
+    if roomBefore != roomAfter:
+        updateDungeon(dungeon, p, p.room_x, p.room_y)
+        for r in dungeon:
+            print(r)
+        print('\n\n')
 
 
 
 
     screen.fill('black')
+    for row in dungeon:
+        for room in row:
+            if room is not None:
+                room.draw()
     p.draw()
     clock.tick(60)
     pg.display.flip()
