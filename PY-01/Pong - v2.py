1729b38f80a11fedf538f2c30f279e0e37ed860e
R-kaloustian <102569106+Roupstrer02@users.noreply.github.com>
2026-08-14 12:23:47 -0400

    file restructuring

---
+++ PY-01/Pong - v2.py
@@ -0,0 +1,106 @@
+import pygame as pg
+pg.init()
+pg.mixer.init()
+screen = pg.display.set_mode((800,600))
+clock = pg.time.Clock()
+
+#colours
+black = (0,0,0)
+blue = (100,100,255)
+red = (255,25,25)
+green = (25,255,25)
+
+#game objects
+player1 = pg.Rect(50,250,25,150)
+player2 = pg.Rect(725,250,25,150)
+player2direction = 0
+ball = pg.Rect(390,290,50,50)
+
+#game object variables
+ballXdirection = 1
+ballYdirection = 1
+ballspeed = 5
+player2speed = 5
+
+#score variables
+player1score = 0
+player2score = 0
+
+#sound effect variables
+hitsound = pg.mixer.Sound("hit.mp3")
+hitsound.set_volume(0.25)
+
+scoresound = pg.mixer.Sound("score.mp3")
+scoresound.set_volume(0.25)
+
+#scoresound = pg.mixer.Sound("find a sound")
+
+#text variables
+font = pg.font.Font(None, 80)
+
+#images
+bluePaddle = pg.image.load("BluePaddle.png")
+redPaddle = pg.image.load("RedPaddle.png")
+yellowBall = pg.image.load("Ball.png")
+
+pg.mixer.music.load("music1.mp3")
+pg.mixer.music.play()
+pg.mixer.music.set_volume(0.25)
+while True:
+    pg.event.pump()
+
+    #inputs
+    mousePos = pg.mouse.get_pos()
+
+    #game logic
+    if player2.y < ball.y - 40:
+        player2direction = 1
+    if player2.y > ball.y - 40:
+        player2direction = -1
+
+    if ball.y < 0:
+        ballYdirection = 1
+    if ball.y > 580:
+        ballYdirection = -1
+
+    if player2.colliderect(ball):
+        ballXdirection = -ballXdirection
+        ballspeed += 1
+        hitsound.play()
+    if player1.colliderect(ball):
+        ballXdirection = -ballXdirection
+        ballspeed += 1
+        hitsound.play()
+        
+    if ball.x < -20:
+        ball.x = 390
+        ball.y = 290
+        player2score += 1
+        ballspeed = 5
+        scoresound.play()
+    if ball.x > 800:
+        ball.x = 390
+        ball.y = 290
+        player1score += 1
+        ballspeed = 5
+        scoresound.play()
+
+    #updates
+    player1.y = mousePos[1] - 50
+    player2.y += player2direction * player2speed
+    ball.x += ballXdirection * ballspeed
+    ball.y += ballYdirection * ballspeed
+    player1scoretext = font.render(str(player1score), True, blue)
+    player2scoretext = font.render(str(player2score), True, red)
+
+    #drawing
+    screen.fill(black)
+    screen.blit(bluePaddle, (player1.x, player1.y))
+    screen.blit(redPaddle, (player2.x - 25, player2.y))
+    screen.blit(yellowBall, (ball.x, ball.y))
+    screen.blit(player1scoretext, (200, 50))
+    screen.blit(player2scoretext, (550, 50))
+    #update frame
+    pg.display.flip()
+    clock.tick(60)
+
--- StringMethods.py
+++
@@ -1,7 +0,0 @@
-firstName = input("Please enter your first name: ")
-
-firstName = firstName.lower()
-print(firstName)
-
-firstName = firstName.capitalize()
-print(firstName)
